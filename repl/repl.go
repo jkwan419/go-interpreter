83c4bf304dc065ef44442473faa4ef0be6d2acd0
Junhee <junheekwan419@gmail.com>
2026-04-22 21:08:02 -0400

    REPL evaluate

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/jkwan419/go-interpreter/evaluator"
 	"github.com/jkwan419/go-interpreter/lexer"
 	"github.com/jkwan419/go-interpreter/parser"
 )
@@ -32,16 +33,22 @@ func Start(in io.Reader, out io.Writer) {
 		if !scanned {
 			return
 		}
+
 		line := scanner.Text()
 		l := lexer.New(line)
 		p := parser.New(l)
+
 		program := p.ParseProgram()
 		if len(p.Errors()) != 0 {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
